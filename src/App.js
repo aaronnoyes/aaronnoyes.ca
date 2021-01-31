b009b28a4ca47140adb19f10a5490cba194be5db
Aaron Noyes <aaron.noyes@me.com>
2021-01-31 02:53:13 -0600

    Design overhaul

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Card from './Card'
 import Header from './Header'
+import Quad from './Quad'
 
 import me from './me.jpeg'
 
@@ -12,24 +13,23 @@ import img_react from './logos/react.png'
 import img_nginx from './logos/nginx.png'
 import img_python from './logos/python.png'
 
+const skills_images = {img_html, img_css, img_js, img_nodejs, img_react, img_nginx, img_python}
+
 function App() {
   return (
     <div className="app">
       <Header></Header>
-      <Card title="Skills">
-        <div className="skills-list">
-          <img src={img_html} className="skills-list__img"></img>
-          <img src={img_css} className="skills-list__img"></img>
-          <img src={img_js} className="skills-list__img"></img>
-          <img src={img_nodejs} className="skills-list__img"></img>
-          <img src={img_react} className="skills-list__img"></img>
-          <img src={img_nginx} className="skills-list__img"></img>
-          <img src={img_python} className="skills-list__img"></img>
-        </div>
+      <Card dim={true}>
+        <h1>Skills</h1>
+      </Card>
+      <Quad a1={img_html} a2={img_css} b1={img_js} b2={img_react}/>
+      <Quad a1={img_nodejs} a2={img_nginx} b1={img_js} b2={img_python}/>
+      <Card dim={true}>
+        <h1>About Me</h1>
       </Card>
       <Card image={me}/>
-      <Card title="About Me">
-        <p>
+      <Card>
+        <p className="bio">
         I am a software developer with a BSc majoring in Computer Science specializing in Networks and Security at the University of Manitoba. Prior to the University of Manitoba attended the University of Alberta where I played football and studied computer science. My primary interests lie in software and network security, as well as web and mobile development. I have been programming as a hobby since 2010 and for my studies since 2015. Most of my school work was written in C/C++, Java, and Python. For personal interest I have also learned about web and iOS development by taking courses on Udemy, and doing self guided learnng in my free time.
         </p>
       </Card>
--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,14 @@
 import './Card.css'
 
 function Card(props) {
+    const inline_style = {backgroundImage: `url(${props.image})`, opacity: "90%"}
+
+    if(props.dim === true) {
+        inline_style.opacity = "100%"
+    }
+
     return (
-        <div className="card" style={{backgroundImage: `url(${props.image})`}}>
+        <div className="card" style={inline_style}>
             <p className="card__title">{props.title}</p>
             <div className="card__content">
                 {props.children}
