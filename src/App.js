0f5599686bb4f03ae8ee3fc7754da56351cf58be
Aaron Noyes <aaron.noyes@me.com>
2021-01-31 11:02:33 -0600

    Reordered main items

--- src/App.js
+++ src/App.js
@@ -20,11 +20,6 @@ function App() {
     <div className="app">
       <Header></Header>
       <Card dim={true}>
-        <h1>Skills</h1>
-      </Card>
-      <Quad a1={img_html} a2={img_css} b1={img_js} b2={img_react}/>
-      <Quad a1={img_nodejs} a2={img_nginx} b1={img_js} b2={img_python}/>
-      <Card dim={true}>
         <h1>About Me</h1>
       </Card>
       <Card image={me}/>
@@ -33,6 +28,11 @@ function App() {
         I am a software developer with a BSc majoring in Computer Science specializing in Networks and Security at the University of Manitoba. Prior to the University of Manitoba attended the University of Alberta where I played football and studied computer science. My primary interests lie in software and network security, as well as web and mobile development. I have been programming as a hobby since 2010 and for my studies since 2015. Most of my school work was written in C/C++, Java, and Python. For personal interest I have also learned about web and iOS development by taking courses on Udemy, and doing self guided learnng in my free time.
         </p>
       </Card>
+      <Card dim={true}>
+        <h1>Skills</h1>
+      </Card>
+      <Quad a1={img_html} a2={img_css} b1={img_js} b2={img_react}/>
+      <Quad a1={img_nodejs} a2={img_nginx} b1={img_js} b2={img_python}/>
     </div>
   );
 }
